9bf62dd18f13b9225dcb1f9846bb62f638443f1c
oldben87 <b-webb@outlook.com>
2020-01-28 17:07:58 +0000

    adding newt, seperating logic and css

---
+++ The Newt/pageLogic.js
@@ -0,0 +1,51 @@
+var myAudio = document.getElementById("myAudio");
+myAudio.volume = 0.7;
+var myButton = document.getElementById("playerBtn");
+var playerTime = document.getElementById("playerTime");
+
+function sec2time(timeInSeconds) {
+  var date = new Date(null);
+  date.setSeconds(timeInSeconds);
+  var sec = date.getSeconds();
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  var min = date.getMinutes();
+
+  let timeString = min + ":" + sec;
+
+  return timeString;
+}
+
+function setDuration(e) {
+  var currTime = Math.floor(e.currentTarget.currentTime);
+  var currDur = Math.floor(e.currentTarget.duration);
+
+  playerTime.textContent = "0:00 / " + sec2time(currDur);
+}
+
+function setTime(e) {
+  var currTime = Math.floor(e.currentTarget.currentTime);
+  var currDur = Math.floor(e.currentTarget.duration);
+
+  playerTime.textContent = sec2time(currTime) + " / " + sec2time(currDur);
+}
+function resetPlayer(e) {
+  myButton.src = "./Shape.svg";
+  setDuration(e);
+}
+
+function togglePlay() {
+  console.log("play paused pressed");
+  if (myAudio.paused) {
+    myAudio.play();
+    myButton.src = "./Shape2.svg";
+  } else {
+    myAudio.pause();
+    myButton.src = "./Shape.svg";
+  }
+}
+
+myAudio.addEventListener("loadedmetadata", setDuration);
+myAudio.addEventListener("timeupdate", setTime);
+myAudio.addEventListener("ended", resetPlayer);
